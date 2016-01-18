d0a0acb670c9739d4e64a43f7ae00a12ca609f7e
jeehyekim <iam@jeehyekim.com>
2016-01-17 20:02:37 -0800

    trying to get contact form to work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,27 @@
 class WelcomeController < ApplicationController
-  def index
+  # def index
+  # 	render :index
+  # end
+
+  def new
+    @contact = Contact.new
   end
+
+  def create
+    @contact = Welcome.new(params[:welcome])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render :new
+    end
+  end
+
+  # private
+
+  # def _params
+  # 	params.require(:contact).permit(:name, :email, :message)
+  # end
+
 end
