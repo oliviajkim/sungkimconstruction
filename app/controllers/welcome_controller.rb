a5cbe66a85b4a21d873d87b55311a05ebb07e0ad
jeehyekim <iam@jeehyekim.com>
2016-01-30 18:11:24 -0800

    added contact and about sections

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,22 +1,22 @@
 class WelcomeController < ApplicationController
-  # def index
-  # 	render :index
+  def index
+  	render :index
+  end
+
+  # def new
+  #   @contact = Contact.new
   # end
 
-  def new
-    @contact = Contact.new
-  end
-
-  def create
-    @contact = Welcome.new(params[:welcome])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
-    else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
-    end
-  end
+  # def create
+  #   @contact = Welcome.new(params[:welcome])
+  #   @contact.request = request
+  #   if @contact.deliver
+  #     flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+  #   else
+  #     flash.now[:error] = 'Cannot send message.'
+  #     render :new
+  #   end
+  # end
 
   # private
 
